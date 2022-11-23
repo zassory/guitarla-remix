4446736620bf29476c04606708be08c41f8c11a1
Nicolas <nicolas.programador@gmail.com>
2022-11-23 19:34:01 -0300

    New Route, carrito de compras

--- app/components/navegacion.jsx
+++ app/components/navegacion.jsx
@@ -1,4 +1,5 @@
 import { Link , useLocation } from '@remix-run/react';
+import imagen from '../../public/img/carrito.png';
 
 export const Navegacion = () => {
 
@@ -6,26 +7,31 @@ export const Navegacion = () => {
 
   return (
     <nav className="navegacion">
-                <Link
-                    to="/"
-                    className={location.pathname === '/' ? 'active' : ''}
-                >Inicio
-                </Link>
-                <Link 
-                    to="/nosotros"
-                    className={location.pathname === '/nosotros' ? 'active' : ''}
-                >Nosotros
-                </Link>
-                <Link 
-                    to="/guitarras"
-                    className={location.pathname === '/guitarras' ? 'active' : ''}
-                >Tienda
-                </Link>
-                <Link 
-                    to="/blog"
-                    className={location.pathname === '/post' ? 'active' : ''}
-                >Blog
-                </Link>
+      <Link
+        to="/"
+        className={location.pathname === '/' ? 'active' : ''}
+      >Inicio
+      </Link>
+      <Link
+        to="/nosotros"
+        className={location.pathname === '/nosotros' ? 'active' : ''}
+      >Nosotros
+      </Link>
+      <Link
+        to="/guitarras"
+        className={location.pathname === '/guitarras' ? 'active' : ''}
+      >Tienda
+      </Link>
+      <Link 
+        to="/blog"
+        className={location.pathname === '/blog' ? 'active' : ''}
+      >Blog
+      </Link>
+      <Link
+        to="/carrito"
+      >
+        <img src={ imagen } alt="carrito de compras" />
+      </Link>
     </nav>
   )
 }
